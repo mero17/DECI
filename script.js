c3760b7a833080563966fe2e0408a408802d6f54
mero17 <g3mero2007@gmail.com>
2025-09-07 23:00:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const toggleButton = document.getElementById("theme-change");
 const body = document.body;
-const div1 = document.getElementById("cont1")
-const div2 = document.getElementById("cont2")
-const div3 = document.getElementById("cont3")
-const link_1 = document.getElementById("link_1")
-const link_2 = document.getElementById("link_2")
+const allContainers = document.querySelectorAll("div")
+allContainers.forEach(element => {
+    element.classList.add("hidden")
+    element.classList.add("common")
+})
+const all_links = document.querySelectorAll("a")
+all_links.forEach(element => {
+    element.classList.add("common")
+})
+const all = document.querySelectorAll(".common")
+const hidden = document.querySelectorAll(".hidden")
+// var rect = div4.getBoundingClientRect()
+const showBox = window.innerHeight * 0.8
 function themeSwitch()
 {
     body.classList.toggle("dark-mode")
-    div1.classList.toggle("dark-mode")
-    div2.classList.toggle("dark-mode")
-    div3.classList.toggle("dark-mode")
-    link_1.classList.toggle("dark-mode")
-    link_2.classList.toggle("dark-mode")
+    all.forEach(element => {
+        element.classList.toggle("dark-mode")
+    })
     if (body.classList.contains("dark-mode"))
     {
         toggleButton.textContent = "☀️";
@@ -21,6 +27,22 @@ function themeSwitch()
     {
         toggleButton.textContent = "🌙";
     }
+}
+function showElement()
+{
+    hidden.forEach(element => {
+        if (element.getBoundingClientRect().top < showBox)
+        {
+            element.classList.remove("hidden")
+        }
+        else
+        {
+            element.classList.add("hidden")
+        }
+    })
+    // alert(rect.top)
     
 }
 toggleButton.addEventListener("click", themeSwitch);
+window.addEventListener('scroll', showElement)
+showElement()
